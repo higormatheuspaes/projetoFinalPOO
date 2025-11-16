64573b7f5bf28f539e870f8fc5a8dc2e973d8dcb
higormatheuspaes <higormatheuspaes11@gmail.com>
2025-11-15 23:23:32 -0300

    fix/limpando projeto

--- Main.java
+++
@@ -1,9 +0,0 @@
-class Main {
-    public static void main(String[] args) {
-        System.out.println("Projetasso de Java funcionando!");
-
-        System.out.println("Teste do Gabriel!");
-
-        System.out.println("branch");
-    }
-}
