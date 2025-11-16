8e1aa2a37820a6983b335931b6d51a05d67369e9
BeckerAC <anaclarakasburg@gmail.com>
2025-11-15 21:07:09 -0300

    alteracaoAdrian

--- Main.java
+++ Main.java
@@ -1,5 +1,7 @@
 class Main {
     public static void main(String[] args) {
         System.out.println("Projetasso de Java funcionando!");
+
+        System.out.println("teste do Adrian!! sou lindasso");
     }
 }
